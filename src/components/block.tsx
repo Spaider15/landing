b36ee4afd822f79f2778bc24129802ad3b1e8734
ia.busarov <ia.busarov@tensor.ru>
2017-08-04 15:39:48 +0300

    add images

---
+++ src/components/block.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const startImage = require("./cwebs_star.png");
+
+export default function() {
+    return (
+        <img src={startImage} />
+    );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import "./index.css";
+import Block from "./components/block";
 
 ReactDOM.render(
     <div>
-        This is 1c landing page
+        <Block />
     </div>,
   document.getElementById("root") as HTMLElement,
 );
