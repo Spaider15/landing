3d828fb95d5772b6011eaad516ac587c5b89cea8
ia.busarov <ia.busarov@tensor.ru>
2017-08-04 14:48:45 +0300

    init

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import "./index.css";
+
+ReactDOM.render(
+    <div>
+        This is 1c landing page
+    </div>,
+  document.getElementById("root") as HTMLElement,
+);
